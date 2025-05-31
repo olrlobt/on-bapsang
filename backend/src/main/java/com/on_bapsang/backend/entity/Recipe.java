c2669534a147808e46c12a81a62adc9f9862d151
chico <silverstuff@naver.com>
2025-05-31 23:25:19 +0900

    fix/merging

--- backend/src/main/java/com/on_bapsang/backend/entity/Recipe.java
+++ backend/src/main/java/com/on_bapsang/backend/entity/Recipe.java
@@ -1,21 +1,41 @@
 package com.on_bapsang.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 
 @Entity
-@Table(name = "`Recipe`")
+@Table(name = "Recipe")
 @Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Recipe {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "recipe_id")
-    private Long id;
+    @Column(name = "recipe_id", length = 32)
+    private String recipeId;
 
     @Column(nullable = false)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(columnDefinition = "TEXT")
+    private String review;
+
+    private String time;
+    private String difficulty;
+    private String portion;
+    private String method;
+
+    @Column(name = "material_type")
+    private String materialType;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "instruction", columnDefinition = "TEXT")
+    private String instruction;
 }
