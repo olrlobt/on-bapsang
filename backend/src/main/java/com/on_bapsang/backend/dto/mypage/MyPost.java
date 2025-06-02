d4bea0d1b939373bcc6f2ab7d584a695cf40d79a
chico <silverstuff@naver.com>
2025-06-02 22:20:49 +0900

    fix:docker용

--- backend/src/main/java/com/on_bapsang/backend/dto/mypage/MyPost.java
+++ backend/src/main/java/com/on_bapsang/backend/dto/mypage/MyPost.java
@@ -1,9 +1,13 @@
 package com.on_bapsang.backend.dto.mypage;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
 public class MyPost {
-
     private Long postId;
     private String title;
     private String content;
@@ -13,18 +17,6 @@ public class MyPost {
     private LocalDateTime createdAt;
     private Double x;
     private Double y;
-
-    public MyPost(Long postId, String title, String content, String imageUrl,
-                  int scrapCount, int commentCount, LocalDateTime createdAt) {
-        this.postId = postId;
-        this.title = title;
-        this.content = content;
-        this.imageUrl = imageUrl;
-        this.scrapCount = scrapCount;
-        this.commentCount = commentCount;
-        this.createdAt = createdAt;
-        this.x = x;
-        this.y = y;
-    }
-
 }
+
+
